f62bf9f93dea1259863320a99bcfd7ebf6e4d711
Sondre Indset <sondre2004@hotmail.no>
2024-01-28 19:45:30 +0100

    Delete Oblig1/Bilbruk3.java

--- Oblig1/Bilbruk3.java
+++
@@ -1,8 +0,0 @@
-public class Bilbruk3 {
-    public static void main(String[] args) {
-        Bil3 bil = new Bil3("AA12345");
-        Person person = new Person(bil);
-
-        person.skrivUt();
-    }
-}
